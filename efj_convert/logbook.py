da3ba692a433bca039cd7522795c0bdb30aecb01
Jon Hurst <jon.a@hursts.org.uk>
2024-02-08 11:39:32 +0000

    Split build_logbook

--- efj_convert/logbook.py
+++ efj_convert/logbook.py
@@ -2,7 +2,7 @@ import importlib.resources as res
 import datetime as dt
 import configparser as cp
 
-from efj_parser import Parser
+from efj_parser import Parser, Sector
 
 
 class UnknownAircraftClass(Exception):
@@ -22,6 +22,27 @@ def _get_template():
     return template
 
 
+def _aircraft_class_cells(
+        sector: Sector,
+        ac_classes: cp.SectionProxy,
+        duration: str
+) -> list[str]:
+    if sector.aircraft.class_:
+        aircraft_class = sector.aircraft.class_
+    else:
+        try:
+            aircraft_class = ac_classes[sector.aircraft.type_]
+        except KeyError:
+            raise UnknownAircraftClass(sector.aircraft.type_)
+    if aircraft_class == "mc":
+        return ["", "", duration]
+    if aircraft_class == "spse":
+        return ["✓", "", ""]
+    if aircraft_class == "spme":
+        return ["", "✓", ""]
+    raise UnknownAircraftClass(sector.aircraft.type_)
+
+
 def build_logbook(in_: str, ac_classes: cp.SectionProxy) -> str:
     _, sectors = Parser().parse(in_)
     rows = []
@@ -32,21 +53,7 @@ def build_logbook(in_: str, ac_classes: cp.SectionProxy) -> str:
                  f"{s.start + dt.timedelta(minutes=s.total):%H%M}",
                  s.aircraft.type_, s.aircraft.reg]
         duration = f"{s.total // 60}:{s.total % 60:02}"
-        if s.aircraft.class_:
-            aircraft_class = s.aircraft.class_
-        else:
-            try:
-                aircraft_class = ac_classes[s.aircraft.type_]
-            except KeyError:
-                raise UnknownAircraftClass(s.aircraft.type_)
-        if aircraft_class not in {"mc", "spse", "spme"}:
-            raise UnknownAircraftClass(s.aircraft.type_)
-        if aircraft_class == "mc":
-            cells.extend(["", "", duration])
-        elif aircraft_class == "spse":
-            cells.extend(["✓", "", ""])
-        else:
-            cells.extend(["", "✓", ""])
+        cells.extend(_aircraft_class_cells(s, ac_classes, duration))
         cells.append(duration)
         cells.append(s.captain)
         cells.extend([str(s.landings.day or ""), str(s.landings.night or "")])
