cf12ebb34066747e7177d514a52536ec16956aea
Jon Hurst <jon.a@hursts.org.uk>
2024-02-19 11:49:18 +0000

    Documented config.py

--- efjtk/config.py
+++ efjtk/config.py
@@ -4,6 +4,14 @@ import configparser as cp
 
 
 def build_config(in_: str, config: str) -> str:
+    """Build a template for an INI file incorporating any unknown types
+
+    :param in_: An eFJ file in string form
+    :param config: An INI file in string form. This can be an empty string or
+        can be the contents of an existing INI file to update.
+    :return: An updated INI file in string form. Any non pre-existing types are
+        added to the [aircraft.classes] section and assigned "spse" as a value.
+    """
     _, sectors = ep.Parser().parse(in_)
     parser = cp.ConfigParser()
     parser.read_string(config)
@@ -18,6 +26,13 @@ def build_config(in_: str, config: str) -> str:
 
 
 def aircraft_classes(config: str) -> cp.SectionProxy:
+    """Extract the [aircraft.classes] section from an INI string.
+
+    :param config: An INI file in string form
+    :return: A ConfigParser SectionProxy object. This can be treated as a non
+        case-sensitive dict, with the aircraft type as key and its category as
+        value.
+    """
     parser = cp.ConfigParser()
     parser.read_string(config)
     return parser["aircraft.classes"]
