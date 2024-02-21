d6bda7c9cdd81e9b334954360dad1830448b1456
Jon Hurst <jon.a@hursts.org.uk>
2024-02-21 14:35:33 +0000

    wip-2

--- efjtk/gui.py
+++ efjtk/gui.py
@@ -6,6 +6,7 @@ import os.path
 import ctypes
 import json
 
+import efjtk.modify
 
 SETTINGS_FILE = os.path.expanduser("~/.efjtkguirc")
 
@@ -14,7 +15,8 @@ class TextWithSyntaxHighlighting(tk.Text):
 
     def __init__(self, parent, highlight_mode, **kwargs):
         tk.Text.__init__(self, parent, background='white',
-                         wrap="none", **kwargs)
+                         wrap="none", undo=True, autoseparators=False,
+                         **kwargs)
         self.highlight_mode = highlight_mode
         self.tag_configure("grayed", foreground="#707070")
         self.tag_configure("keyword", foreground="green")
@@ -90,8 +92,12 @@ class MainWindow(tk.Tk):
             self.settings = {}
         tk.Tk.__init__(self)
         self.title("efjtk")
+        self.menus = {}
         self.__make_menu()
+        self.menus["edit"].entryconfigure("Undo", state="disabled")
+        self.menus["edit"].entryconfigure("Redo", state="disabled")
         self.__make_widgets()
+        self.txt.bind("<<UndoStack>>", self.__manage_undo)
 
     def destroy(self):
         with open(SETTINGS_FILE, "w") as f:
@@ -113,6 +119,7 @@ class MainWindow(tk.Tk):
 
     def __make_menu(self):
         top = tk.Menu(self)
+        self.menus["top"] = top
         self.config(menu=top)
         self.__make_menu_section(top, "File", (
             ('Open', self.__open),
@@ -121,14 +128,15 @@ class MainWindow(tk.Tk):
             ('Quit', self.quit),
         ))
         self.__make_menu_section(top, "Edit", (
-            ('Undo', self.__not_implemented),
+            ('Undo', self.__undo),
+            ('Redo', self.__redo),
             ("", None),
             ('Clear', self.__not_implemented),
         ))
         self.__make_menu_section(top, "Modify", (
-            ('Expand', self.__not_implemented),
-            ('Night', self.__not_implemented),
-            ('FO', self.__not_implemented),
+            ('Expand', self.__expand),
+            ('Night', self.__night),
+            ('FO', self.__fo),
             ('VFR', self.__not_implemented),
             ('Instructor', self.__not_implemented),
         ))
@@ -139,6 +147,7 @@ class MainWindow(tk.Tk):
 
     def __make_menu_section(self, top, label, entries, underline=0):
         menu = tk.Menu(top, tearoff=0)
+        self.menus[label.lower()] = menu
         for entry_label, callback in entries:
             if entry_label:
                 menu.add_command(label=entry_label,
@@ -164,6 +173,53 @@ class MainWindow(tk.Tk):
             self.txt.delete("1.0", tk.END)
             self.txt.insert("1.0", efj)
             self.txt.see(tk.END)
+
+    def __expand(self):
+        self.__modify(efjtk.modify.expand_efj)
+
+    def __night(self):
+        self.__modify(efjtk.modify.add_night_data)
+
+    def __fo(self):
+        self.__modify(efjtk.modify.add_fo_role_flag)
+
+    def __modify(self, fn):
+        text = self.txt.get('1.0', 'end')
+        result = fn(text)
+        range_ = self.txt.tag_ranges("sel")
+        if range_:
+            start_line = int(self.txt.index(range_[0]).split(".")[0]) - 1
+            end_line = int(self.txt.index(range_[1]).split(".")[0]) - 1
+            text_lines = text.splitlines()
+            result_lines = result.splitlines()
+            result = "\n".join(text_lines[:start_line] +
+                               result_lines[start_line:end_line]
+                               + text_lines[end_line:])
+        self.txt.edit_separator()
+        pos = self.txt.index(tk.INSERT)
+        self.txt.delete('1.0', tk.END)
+        self.txt.insert('1.0', result)
+        self.txt.mark_set(tk.INSERT, pos)
+
+    def __undo(self):
+        if self.txt.edit("canundo"):
+            self.txt.edit_undo()
+            self.txt.highlight_syntax()
+
+    def __redo(self):
+        if self.txt.edit("canredo"):
+            self.txt.edit_redo()
+            self.txt.highlight_syntax()
+
+    def __manage_undo(self, event):
+        if self.txt.edit("canundo"):
+            self.menus["edit"].entryconfigure("Undo", state="normal")
+        else:
+            self.menus["edit"].entryconfigure("Undo", state="disabled")
+        if self.txt.edit("canredo"):
+            self.menus["edit"].entryconfigure("Redo", state="normal")
+        else:
+            self.menus["edit"].entryconfigure("Redo", state="disabled")
 
 
 def main():
