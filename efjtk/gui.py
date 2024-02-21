697eb98f96e84ab9c00631f47dc43a3fb3d98bf1
Jon Hurst <jon.a@hursts.org.uk>
2024-02-21 10:42:11 +0000

    Added syntax highlighting

--- efjtk/gui.py
+++ efjtk/gui.py
@@ -1,13 +1,83 @@
 import tkinter as tk
 from tkinter import ttk
 # from tkinter import messagebox
-# from tkinter import filedialog
+from tkinter import filedialog
 import os.path
 import ctypes
 import json
 
 
 SETTINGS_FILE = os.path.expanduser("~/.efjtkguirc")
+
+
+class TextWithSyntaxHighlighting(tk.Text):
+
+    def __init__(self, parent, highlight_mode, **kwargs):
+        tk.Text.__init__(self, parent, background='white',
+                         wrap="none", **kwargs)
+        self.highlight_mode = highlight_mode
+        self.tag_configure("grayed", foreground="#707070")
+        self.tag_configure("keyword", foreground="green")
+        self.tag_configure("datetime", foreground="blue")
+        self.bind(
+            '<KeyRelease>',
+            lambda *args: self.edit_modified() and self.highlight_syntax())
+
+    def insert(self, idx, text, *args):
+        tk.Text.insert(self, idx, text, *args)
+        self.highlight_syntax()
+
+    def highlight_syntax(self):
+        if not self.highlight_mode:
+            return
+        for tag in ("keyword", "datetime", "grayed"):
+            self.tag_remove(tag, "1.0", "end")
+        if self.highlight_mode == 'efj':
+            self.highlight_efj()
+        self.edit_modified(False)
+
+    def highlight_efj(self):
+        count = tk.IntVar()
+        start_idx = "1.0"
+        while True:
+            new_idx = self.search(
+                r"\d{4}-\d{2}-\d{2}",
+                start_idx, count=count, regexp=True,
+                stopindex="end")
+            if not new_idx:
+                break
+            start_idx = f"{new_idx} + {count.get()} chars"
+            self.tag_add("datetime", new_idx, start_idx)
+        start_idx = "1.0"
+        while True:
+            new_idx = self.search(
+                r"\d{4}/\d{4}",
+                start_idx, count=count, regexp=True,
+                stopindex="end")
+            if not new_idx:
+                break
+            start_idx = f"{new_idx} + {count.get()} chars"
+            self.tag_add("datetime", new_idx, start_idx)
+        start_idx = "1.0"
+        while True:
+            new_idx = self.search(
+                "CP:|FO:|PU:|FA:",
+                start_idx, count=count, regexp=True,
+                stopindex="end")
+            if not new_idx:
+                break
+            start_idx = f"{new_idx} + {count.get()} chars"
+            self.tag_add("keyword", new_idx, start_idx)
+        start_idx = "1.0"
+        while True:
+            new_idx = self.search(
+                r"#.*",
+                start_idx, count=count, regexp=True,
+                stopindex="end")
+            if not new_idx:
+                break
+            start_idx = f"{new_idx} + {count.get()} chars"
+            self.tag_add("grayed", new_idx, start_idx)
 
 
 class MainWindow(tk.Tk):
@@ -33,18 +103,19 @@ class MainWindow(tk.Tk):
         sby = ttk.Scrollbar(self, orient='vertical')
         sbx.grid(row=1, column=0, sticky=tk.EW)
         sby.grid(row=0, column=1, sticky=tk.NS)
-        self.txt = tk.Text(self)
+        self.txt = TextWithSyntaxHighlighting(self, "efj")
         self.txt.grid(row=0, column=0, sticky=tk.NSEW)
         sbx.config(command=self.txt.xview)
         sby.config(command=self.txt.yview)
         self.txt.config(xscrollcommand=sbx.set)
         self.txt.config(yscrollcommand=sby.set)
+        self.txt.focus()
 
     def __make_menu(self):
         top = tk.Menu(self)
         self.config(menu=top)
         self.__make_menu_section(top, "File", (
-            ('Open', self.__not_implemented),
+            ('Open', self.__open),
             ('Save', self.__not_implemented),
             ("", None),
             ('Quit', self.quit),
@@ -79,6 +150,19 @@ class MainWindow(tk.Tk):
 
     def __not_implemented(self):
         pass
+
+    def __open(self):
+        path = self.settings.get('openPath')
+        fn = filedialog.askopenfilename(
+            filetypes=(("All", "*"),),
+            initialdir=path)
+        if not fn:
+            return
+        self.settings['openPath'] = os.path.dirname(fn)
+        with open(fn) as f:
+            efj = f.read()
+            self.txt.delete("1.0", tk.END)
+            self.txt.insert("1.0", efj)
 
 
 def main():
