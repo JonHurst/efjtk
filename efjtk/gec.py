a12a4c5fa9fd28a92faa8d41b7efb3a1f5eb7b69
Jon Hurst <jon.a@hursts.org.uk>
2026-09-11 19:22:32 +0100

    Use fixed range for reasonable average velocities

--- efjtk/gec.py
+++ efjtk/gec.py
@@ -1,12 +1,12 @@
 import efj_parser as ep
 import datetime as dt
 import math
-import statistics
 
 from nightflight.airport_nvecs import airfields  # type:ignore
 
 GEC_MIN_SECTOR_OVERLAP = 15
-STDDEV_ANOM = 2
+VELOCITY_LIMIT_LOW = 50
+VELOCITY_LIMIT_HIGH = 500
 
 
 def sector_overlaps(sorted_sectors: list[ep.Sector]) -> list[str]:
@@ -22,9 +22,7 @@ def sector_overlaps(sorted_sectors: list[ep.Sector]) -> list[str]:
 
 
 def velocity_anomaly(sectors: list[ep.Sector]) -> tuple[list[str], list[str]]:
-    sectors = [X for X in sectors if X.airports.origin != X.airports.dest]
     unknown_airfields = set()
-    velocities_by_type: dict[str, list[float]] = {}
     results = []
     for s in sectors:
         origin_nvec, dest_nvec = None, None
@@ -41,19 +39,10 @@ def velocity_anomaly(sectors: list[ep.Sector]) -> tuple[list[str], list[str]]:
             angle = math.acos(min(1, sprod))
             distance = 3440 * angle
             if distance > 100:
-                v = distance * 60 / s.total
-                results.append((distance, v, s))
-                velocities_by_type.setdefault(s.aircraft.type_, []).append(v)
-    ranges_by_type = {}
-    for k, vlist in velocities_by_type.items():
-        mean = statistics.mean(vlist)
-        stdev = statistics.stdev(vlist)
-        ranges_by_type[k] = (mean - STDDEV_ANOM * stdev,
-                             mean + STDDEV_ANOM * stdev)
+                results.append((distance, distance * 60 / s.total, s))
     anomolous = []
     for d, v, s in results:
-        r = ranges_by_type[s.aircraft.type_]
-        if not (r[0] < v < r[1]):
+        if not (VELOCITY_LIMIT_LOW < v < VELOCITY_LIMIT_HIGH):
             end = s.start + dt.timedelta(minutes=s.total)
             anomolous.append(
                 f"{s.start:%Y-%m-%d %H%M}/{end:%H%M} "
